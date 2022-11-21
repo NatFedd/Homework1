bd112b6abbfb65541dda663c2daabe890c788230
natfedd <ryabikhina@mail.ru>
2022-11-21 12:26:53 +0500

    conditional operator домашка после видеоразбора

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Домашнее задание 1. Задача 1");
         int ageMajority = 20;
         if (ageMajority > 18) {
-            System.out.println("Вы совершеннолетний");
+            System.out.println("Поздравляем с совершеннолетием");
         }
         if (ageMajority == 18) {
             System.out.println("Поздравляем с днем рождения");
@@ -12,22 +12,22 @@ public class Main {
         if (ageMajority < 18) {
             System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
         }
-        //Задача 2
-        System.out.println("Задача 2");
-        int age = 19;
-        if (age >= 7)
-            if (age < 18) {
-                System.out.println("Ребенок ходит в школу");
-            }
-        if (age >= 18)
-            if (age < 24) {
-                System.out.println("Человек уже закончил школу и может отправляться в университет");
-            }
+
+        //Домашнее задание 1. Задача 2
+        System.out.println("Домашнее задание 1. Задача 2");
+        int age = 9;
+        if (age >= 7 && age < 18) {
+            System.out.println("Ребенок ходит в школу");
+        }
+        if (age >= 18 && age < 24) {
+            System.out.println("Человек уже закончил школу и может отправляться в университет");
+        }
         if (age >= 24) {
             System.out.println("человек окончил университет и ему пора искать первую работу");
         }
-        //Задача 3
-        System.out.println("Задача 3");
+
+        //Домашнее задание 1. Задача 3
+        System.out.println("Домашнее задание 1. Задача 3");
         int thePlaceOccupied = 100;
         int capacityOneTrainCar = 102;
         int sittingPlace = 60;
@@ -46,27 +46,27 @@ public class Main {
         if (thePlaceOccupied == capacityOneTrainCar) {
             System.out.println("В вагоне свободных мест нет");
         }
+
         //Домашнее задание 2. Задача 1
         System.out.println("Домашнее задание 2. Задача 1");
         int ageMajority1 = 8;
         if (ageMajority1 >= 18) {
-            System.out.println("Поздравляем с днем рождения");
+            System.out.println("Поздравляем с совершеннолетием!");
         } else {
             System.out.println("Возраст совершеннолетия ещё не наступил, нужно немного подождать");
         }
+
         //Домашнее задание 2. Задача 2
         System.out.println("Домашнее задание 2. Задача 2");
         int age1 = 8;
-        if (age1 >= 7) {
+        if (age1 >= 7 && age < 18) {
             System.out.println("Ребенок ходит в школу");
         } else {
-            if (age1 >= 18) {
-                if (age1 < 24) {
-                    System.out.println("Человек уже закончил школу и может отправляться в университет");
-                } else {
-                    if (age1 >= 24) {
-                        System.out.println("человек окончил университет и ему пора искать первую работу");
-                    }
+            if (age1 >= 18 && age1 < 24) {
+                System.out.println("Человек уже закончил школу и может отправляться в университет");
+            } else {
+                if (age1 >= 24) {
+                    System.out.println("человек окончил университет и ему пора искать первую работу");
                 }
             }
         }
@@ -92,41 +92,46 @@ public class Main {
         } else {
             System.out.println("В вагоне свободных мест нет");
         }
+
         //Домашнее задание 3. Задача 1
         System.out.println("Домашнее задание 3. Задача 1");
         int age2 = 24;
-        if (age2 >= 2 && age2 < 6)
+        if (age2 >= 2 && age2 <= 6)
             System.out.println("Возраст человека равен " + age2 + ", ему нужно ходить в детский сад");
         if (age2 >= 7 && age2 < 18)
             System.out.println("Возраст человека равен " + age2 + ", ему нужно ходить в школу");
         if (age2 >= 18 && age2 < 24) {
             System.out.println("Возраст человека равен " + age2 + ", ему нужно ходить в университет");
         } else {
-        System.out.println("Возраст человека равен " + age2 + ", ему нужно ходить на работу");
-    }
+            System.out.println("Возраст человека равен " + age2 + ", ему нужно ходить на работу");
+        }
+
         //Домашнее задание 3. Задача 2
         System.out.println("Домашнее задание 3. Задача 2");
-        int age3 = 14;
-        if (age3 < 5)
+        int age3 = 13;
+        if (age3 < 5) {
             System.out.println("Возраст человека равен " + age3 + ", он не может кататться на аттракционе");
-        if (age3 >= 5 && age3 < 14) {
+        } else if (age3 < 14) {
             System.out.println("Возраст человека равен " + age3 + ", он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя");
         } else {
             System.out.println("Возраст человека равен " + age3 + ", он может кататься без сопровождения взрослого");
         }
+
         //Домашнее задание 3. Задача 3
         System.out.println("Домашнее задание 3. Задача 3");
-        int one = 6;
-        int two = 5;
-        int free = 7;
-        if (one < two && two > free)
-            System.out.println("Самым большим числом является two и оно равно " + two);
-        if (two < one && one > free) {
-            System.out.println("Самым большим числом является one и оно равно " + one);
+        int one = 4;
+        int two = 8;
+        int three = 10;
+        if (one >= two && one > three) {
+            System.out.println("Самым большим числом является two и оно равно " + one);
+        } else if (one > two && one <= three) {
+            System.out.println("Самым большим числом является one и оно равно " + three);
+        } else if (two > one && two > three) {
+            System.out.println("Самым большим числом является one и оно равно " + two);
         } else {
-            System.out.println("Самым большим числом является free и оно равно " + free);
+            System.out.println("Самым большим числом является three и оно равно " + three);
         }
 
+    }
 
-
-}}
+}
