4773f7da4edd343d63f4ea5eb662e3eb62070607
natfedd <ryabikhina@mail.ru>
2022-11-28 11:59:25 +0500

    cycles.Part_1 домашка

--- src/Main.java
+++ src/Main.java
@@ -1,70 +1,61 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
-        System.out.println("Задача 1");
-        int clientOS = 1;
-        if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+        //Домашнее задание 1. Задача 1
+        System.out.println("Домашнее задание 1. Задача 1");
+        for (int i = 1; i <= 10; i++) {
+        System.out.println("Итерация цикла " + i);
+    }
+        //Домашнее задание 1. Задача 2
+        System.out.println("Домашнее задание 1. Задача 2");
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Итерация цикла " + i);
         }
-        if (clientOS == 1) {
-            System.out.println("Установите версию приложения для Android  по ссылке");
+        //Домашнее задание 1. Задача 3
+        System.out.println("Домашнее задание 1. Задача 3");
+        for (int i = 0; i < 17; i+=2) {
+            System.out.println("Итерация цикла " + i);
         }
-        //Задача 2
-        System.out.println("Задача 2");
-        int clientOS1 = 0;
-        int produced = 2014;
-        if (clientOS1 == 0 && produced < 2015) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (clientOS1 == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if (produced < 2015) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else {
-            System.out.println("Установите версию приложения для Android по ссылке");
+        //Домашнее задание 1. Задача 4
+        System.out.println("Домашнее задание 1. Задача 4");
+        for (int i = 10; i >= -10; i--) {
+            System.out.println("Итерация цикла " + i);
         }
-        //Задача 3
-        System.out.println("Задача 3");
-        int year = 1804;
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)  {
-            System.out.println(year + " год - висосный");
-        } else {
-            System.out.println(year +" год не является висосным");
+        //Домашнее задание 2. Задача 1
+        System.out.println("Домашнее задание 2. Задача 1");
+        for (int i = 1904; i <= 2096; i+=4) {
+            System.out.println(i + " год является високосным " );
         }
-        //Задача 4
-        System.out.println("Задача 4");
-        int deliveryDistance = 60;
-        int deliveryDays = 1;
-        if (deliveryDistance <= 20){
-            System.out.println("Потребуется " + deliveryDays + " день для доставки карты");
+        //Домашнее задание 2. Задача 2
+        System.out.println("Домашнее задание 2. Задача 2");
+        for (int i = 7; i <= 98; i+=7) {
+            System.out.print(i + " " );
         }
-        else {
-            System.out.println("Потребуется " + (((deliveryDistance - 20) / 40 + deliveryDays+1)) + " дня(-ей) для доставки карты") ;
+        System.out.println();
+        //Домашнее задание 2. Задача 3
+        System.out.println("Домашнее задание 2. Задача 3");
+        int number = 1;
+            System.out.print(number+ " ");
+        for (int i = 0; i < 9; i++) {
+            number = number * 2;
+            System.out.print(number + " ");
         }
-        //Задача 5
-        System.out.println("Задача 5");
-        int monthNumber = 14;
-        switch (monthNumber) {
-            case 1:
-            case 2:
-            case 12:
-            System.out.println("Зимний месяц");
-                break;
-            case 3:
-            case 4:
-            case 5:
-                System.out.println("Весенний месяц");
-                break;
-            case 6:
-            case 7:
-            case 8:
-                System.out.println("Летний месяц");
-                break;
-            case 9:
-            case 10:
-            case 11:
-                System.out.println("Осенний месяц");
-                default:
-                    System.out.println("Такого месяца не существует");
+        System.out.println();
+        //Домашнее задание 3. Задача 1
+        System.out.println("Домашнее задание 3. Задача 1");
+        int accumulation = 29000;
+        int total1 = 0;
+        for (int i=0; i < 12; i++) {
+            total1 = total1 + accumulation;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
+        }
+        //Домашнее задание 3. Задача 2
+        System.out.println("Домашнее задание 3. Задача 2");
+        int accumulation1 = 29000;
+        float total2 = 0;
+        for (int i=0; i < 12; i++) {
+            total2 = total2 + total2/100;
+            total2 = total2 + accumulation1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total2 + " рублей");
         }
     }
 }
