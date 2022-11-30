d5c11df8c4369fd2a657686e0b692d7ba9f682bb
natfedd <ryabikhina@mail.ru>
2022-11-30 11:31:32 +0500

    cycles.Part_2 30.11

--- src/Main.java
+++ src/Main.java
@@ -35,6 +35,59 @@ public class Main {
             population = population + birthRate + mortality;
             System.out.println("Год " + years + ", численность населения составляет " + population + " человек");
         }
+
+        //Домашнее задание 2. Задача 1
+        System.out.println("Домашнее задание 2. Задача 1");
+        float accumulation1 = 15000;
+        for (int month = 1; accumulation1 < 12_000_000; month++) {
+            accumulation1 = accumulation1 + accumulation1 / 100 * 7;
+            System.out.println("Месяц " + month + ". Сумма накоплений равна " + accumulation1 + " рублей");
+        }
+
+        //Домашнее задание 2. Задача 2
+        System.out.println("Домашнее задание 2. Задача 2");
+        float accumulation2 = 15000;
+        for (int month = 1; accumulation2 < 12_000_000; month++) {
+            accumulation2 = accumulation2 + accumulation2 / 100 * 7;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ". Сумма накоплений равна " + accumulation2 + " рублей");
+            }
+        }
+
+        //Домашнее задание 2. Задача 3
+        System.out.println("Домашнее задание 2. Задача 3");
+        float accumulation3 = 15000;
+        for (int month = 1; month < 108; month++) {
+            accumulation3 = accumulation3 + accumulation3 / 100 * 7;
+            if (month % 6 == 0) {
+                System.out.println("Месяц " + month + ". Сумма накоплений равна " + accumulation3 + " рублей");
+            }
+
+        }
+        //Домашнее задание 2. Задача 4
+        System.out.println("Домашнее задание 2. Задача 4");
+        int friday = 3;
+        for (int day = 1; day <= 31; day++) {
+            if ((day - friday) % 7 == 0)
+                System.out.println("Сегодня пятница, " + day + "-е число. Необходимо подготовить отчет.");
+        }
+        //Домашнее задание 3. Задача 1
+        System.out.println("Домашнее задание 3. Задача 1");
+        int now = 2022;
+        int y1Before = 200;
+        int y1After = 100;
+        for (int y1 = now - y1Before; y1 < (now + y1After); y1++) {
+            if (y1 % 79 == 0) {
+                System.out.println(y1);
+
+        }
+            //Домашнее задание 3. Задача 2
+            System.out.println("Домашнее задание 3. Задача 2");
+            int figure = 2;
+            for (int fig1 = 1; fig1 <= 10 ; fig1++)
+        System.out.println(figure + "*" + fig1 + "=" + (figure*fig1));
+        }
+        }
+
     }
 
-}
